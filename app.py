d76a59a3e63c8c888675fff5c525508b1b5f739e
Tom Snelling <tomsnelling8@gmail.com>
2016-06-08 22:02:56 +0100

    aesthetics

--- app.py
+++ app.py
@@ -4,9 +4,15 @@
 # YEAR 2016
 
 from flask import Flask, render_template, request
-import pcarsListener, json, os
+import pcarsListener, json
 
 app = Flask(__name__)
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
 @app.route('/')
 def index():
@@ -91,8 +97,8 @@ def getData():
 @app.route('/kill')
 def kill():
 	pcarsListener.terminate()
-	os.system('taskkill /im python.exe /t /F') # HORRIBLE TEMPORARY HACK
-	return 0
+	shutdown_server()
+	return "OK", 200
 
 if __name__ == "__main__":
 	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
